7e19820fc3fdc2e3d447746230647c71f0df7b97
moahelmy <muhammad.cs@gmail.com>
2016-06-02 05:35:28 +1000

    Delete IQuery and upgrade NH

--- Swart.Repositories.NHibernate/Repository.cs
+++ Swart.Repositories.NHibernate/Repository.cs
@@ -7,7 +7,7 @@ using Swart.DomainDrivenDesign.Repositories;
 namespace Swart.Repositories.NHibernate
 {
     public class Repository<TEntity, TKey>:RepositoryBase<TEntity, TKey> 
-        where TKey : IComparable where TEntity : class, IEntity<TKey>
+        where TKey : IComparable, IEquatable<TKey> where TEntity : class, IEntity<TKey>
     {        
         private readonly ISessionUnitOfWork _unitOfWork;
 
@@ -16,12 +16,12 @@ namespace Swart.Repositories.NHibernate
             _unitOfWork = unitOfWork;
         }
 
-        #region Basic
-        public override IQueryable<TEntity> List()
+        protected override IQueryable<TEntity> _List()
         {
             return _unitOfWork.Session.Query<TEntity>();
         }
 
+        #region Basic
         public override TEntity Get(TKey id)
         {
             return _unitOfWork.Session.Get<TEntity>(id);
@@ -29,17 +29,18 @@ namespace Swart.Repositories.NHibernate
         #endregion
 
         #region List
-        public override void Add(TEntity entity)
+
+        protected override void AddEntity(TEntity entity)
         {
             _unitOfWork.Session.Save(entity);
         }
 
-        public override void Delete(TEntity entity)
+        protected override void DeleteEntity(TEntity entity)
         {
             _unitOfWork.Session.Delete(entity);
         }
 
-        public override void Update(TEntity entity)
+        protected override void UpdateEntity(TEntity entity)
         {
             _unitOfWork.Session.Update(entity);            
         }
